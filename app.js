7f4245e1e95f42fc90387fb7c0baaf01af009832
Yeicob David <yeicob36@gmail.com>
2025-08-08 21:06:35 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,48 @@
+/*
+//contador 1 a 10 
+
+    let Numero = 0;
+
+    while (Numero != 10){
+        Numero = Numero + 1;
+        console.log (Numero);
+        alert (Numero);
+    }
+ 
+
+//contador 10 a 1
+    let Numero2 = 10;
+    while (Numero2 != 0)
+        {
+        Numero2 = Numero2 -1;
+        console.log(Numero2);
+        alert(Numero2);
+        }
+
+//cuenta regresiva
+    let numeroMax = prompt('Cuenta regresiva inicia en');
+
+    while(numeroMax !=0) {
+        console.log(numeroMax);
+        alert(numeroMax);
+        numeroMax = numeroMax -1;
+}
+
+//cuenta progresiva
+
+let numeroContar =  parseInt(prompt('Ingresa un número para iniciar el conteo'));
+let contador = 0;
+
+
+if (isNaN(numeroContar)) {
+    alert('Por favor, ingresa un número válido.');
+} else {
+    
+    while (contador <= numeroContar) {
+        console.log(contador);
+        alert(contador);
+        contador = contador + 1;
+    }
+    alert('Conteo terminado');
+}
+*/
